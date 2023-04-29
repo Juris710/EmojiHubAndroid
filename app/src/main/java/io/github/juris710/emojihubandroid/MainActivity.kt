ff7748c53a925e078ba05070b35ec6d74cedad25
Juris710 <juris710.dev@gmail.com>
2023-04-29 11:06:15 +0900

    Use LazyColumn instead of Column to show emojis

--- app/src/main/java/io/github/juris710/emojihubandroid/MainActivity.kt
+++ app/src/main/java/io/github/juris710/emojihubandroid/MainActivity.kt
@@ -6,8 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -206,13 +206,9 @@ class MainActivity : ComponentActivity() {
             scrollState.scrollTo(0)
         }
         CategoryChips(selectedCategory) { selectedCategory = it }
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-                .verticalScroll(scrollState)
-        ) {
-            emojis.forEach {
-                EmojiDisplay(emoji = it)
+        LazyColumn {
+            items(emojis.size) {
+                EmojiDisplay(emoji = emojis[it])
             }
         }
     }
