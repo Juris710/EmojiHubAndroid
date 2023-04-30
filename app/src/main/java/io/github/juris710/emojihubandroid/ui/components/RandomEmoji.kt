f0c075e40300268fe23f2c14284a98b23881cc3a
Juris710 <juris710.dev@gmail.com>
2023-04-30 17:13:56 +0900

    Add CircularProgressIndicator in RandomEmoji component

--- app/src/main/java/io/github/juris710/emojihubandroid/ui/components/RandomEmoji.kt
+++ app/src/main/java/io/github/juris710/emojihubandroid/ui/components/RandomEmoji.kt
@@ -1,9 +1,16 @@
 package io.github.juris710.emojihubandroid.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import io.github.juris710.emojihubandroid.data.Emoji
 import io.github.juris710.emojihubandroid.data.HttpResult
 
@@ -12,11 +19,22 @@ fun RandomEmoji(
     randomEmoji: HttpResult<Emoji>?,
     getRandomEmoji: () -> Unit
 ) {
-    val emoji = when (randomEmoji) {
-        is HttpResult.Success -> randomEmoji.data
-        else -> null
+    if (randomEmoji is HttpResult.Loading) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .size(64.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        val emoji = when (randomEmoji) {
+            is HttpResult.Success -> randomEmoji.data
+            else -> null
+        }
+        EmojiDisplay(emoji)
     }
-    EmojiDisplay(emoji)
     val buttonEnabled = randomEmoji == null || randomEmoji !is HttpResult.Loading
     Button(onClick = getRandomEmoji, enabled = buttonEnabled) {
         Text(text = "Show Random Emoji")
